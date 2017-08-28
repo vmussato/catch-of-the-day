61c05bab24c27f42c5920da56cd532fdf2135089
Vinicius Mussato <vinicius.mussato@gmail.com>
2017-08-27 21:46:25 -0300

    Fishes sample  are now being rendered

--- scripts/main.js
+++ scripts/main.js
@@ -33,11 +33,17 @@ var App = React.createClass({
 			fishes : require('./sample-fishes')
 		});
 	},
+	renderFish : function(key){
+		return <Fish key={key} index={key} details={this.state.fishes[key]} />
+	},
 	render : function() {
 		return(
 			<div className="catch-of-the-day">
 				<div className="menu">
 					<Header tagline="Fresh Seafood Market"/>
+					<ul className="list-of-fishes">
+						{Object.keys(this.state.fishes).map(this.renderFish)}
+					</ul>
 				</div>
 				<Order />
 				<Inventory addFish={this.addFish} loadSamples={this.loadSamples}/>
@@ -46,6 +52,29 @@ var App = React.createClass({
 	}
 });
 
+
+/*
+	Fish
+
+
+*/
+
+var Fish = React.createClass({
+	render : function() {
+		var details = this.props.details;
+		return (
+			<li className="menu-fish">
+				<img src={this.props.details.image}  alt="" />
+				<h3 className="fish-name">
+					{details.name}
+					<span className="price">{h.formatPrice(details.price)}</span>
+				</h3> 
+				<p>{details.desc}</p>
+			</li> 
+		)
+	}
+
+});
 /*
 	Add Fish Form
 */
